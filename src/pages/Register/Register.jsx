14cfb574ee19a6661e3523e804f81e2acd205c7e
dkm3689 <dkm1480@gmail.com>
2025-01-26 09:51:26 +0530

    styled components implemented

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,34 +1,48 @@
 import React from 'react'
 // import "./Register.css"
 import styles from "./Register.module.css"
+import styled from "styled-components"
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: ${(props) => (props.color) || "#BF4F74" };
+  &:hover{ color: green};
+  `
 
 const Register = () => {
   return (
     <>
     <div className={styles.registerContainer}>
       
-      <div className={styles.heading}>
+      <div className={styles.headingBox}>
         <h2> Register </h2>
       </div>
 
-      <div>
-        <label for="usernameInput" className={styles.label}> Username </label>
-        <input  type="text" id="usernameInput"/>
-      </div>
+      <Title> Styled Component </Title>
+      <Title color="red"> Styled Component </Title>
 
-      <div>
-        <label for="emailInput" className={styles.label}> Email </label>
-        <input  type="text" id="emailInput" />
-      </div>
+      <form>
+        <div className={styles.inputBox}>
+          <label for="usernameInput" className={styles.label}> Username </label>
+          <input  type="text" id="usernameInput"/>
+        </div>
 
-      <div>
-        <label for="passwordInput" className={styles.label}> Password </label>
-        <input  type="password" id="passwordInput" />
-      </div>
+        <div className={styles.inputBox}>
+          <label for="emailInput" className={styles.label}> Email </label>
+          <input  type="text" id="emailInput" />
+        </div>
 
-      <div>
-        <button> Register </button>
-      </div>
+        <div className={styles.inputBox}>
+          <label for="passwordInput" className={styles.label}> Password </label>
+          <input  type="password" id="passwordInput" />
+        </div>
+
+        <div className={styles.buttonBox}>
+          <button> Register </button>
+        </div>
+
+      </form>
 
     </div>
   </>
